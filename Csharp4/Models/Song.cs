896ce35373b3fe3612c6bbdf5fc6cc5890a6d667
gabrielfxz <gabrielperozim7@gmail.com>
2023-07-03 17:06:51 -0300

    models

---
+++ Csharp4/Models/Song.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Csharp4.Models
+{
+    internal class Song
+    {
+        [JsonPropertyName("artist")]
+        public string? Artist { get; set; }
+
+        [JsonPropertyName("song")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public int Duration { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string? Genre { get; set; }
+     
+        public void ShowSongDetails()
+        {
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Duration: {Duration/1000}");
+            Console.WriteLine($"Genre: {Genre}");
+
+        }
+    }
+}
--- Csharp4/Program.cs
+++ Csharp4/Program.cs
@@ -1,1 +1,18 @@
-﻿//https://guilhermeonrails.github.io/api-csharp-songs/songs.json
+﻿using Csharp4.Models;
+using System.Text.Json;
+
+using (HttpClient  client = new HttpClient())
+{
+    try
+    {
+        string answer = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(answer);
+        var songs = JsonSerializer.Deserialize<List<Song>>(answer);
+        songs[999].ShowSongDetails();
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"we got a problem: {ex.Message}");
+    }
+    
+}
